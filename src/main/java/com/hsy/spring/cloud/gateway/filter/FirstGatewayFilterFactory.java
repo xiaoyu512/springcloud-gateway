2727af6f7e5282b377048c4c14648b8bd4d96a87
huoshengyu <915967363@qq.com>
2020-04-08 20:21:03 +0800

    HSY-顺序测试

---
+++ src/main/java/com/hsy/spring/cloud/gateway/filter/FirstGatewayFilterFactory.java
@@ -0,0 +1,58 @@
+package com.hsy.spring.cloud.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 请求前置过滤器
+ *
+ * @author HuoShengyu
+ * @version 1.0
+ * @date 2019-11-28 14:40:01
+ */
+@Slf4j
+@Order(6)
+@Component
+public class FirstGatewayFilterFactory extends AbstractGatewayFilterFactory<FirstGatewayFilterFactory.Config> {
+    public static final String ENABLED_KEY = "enabled";
+
+    public FirstGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return Collections.singletonList(ENABLED_KEY);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            log.info("执行过滤器[{}]", this.getClass().getName());
+            MultiValueMap<String, String> queryParams = exchange.getRequest().getQueryParams();
+            queryParams.forEach((key, value) -> {
+                log.info("~~~~~~{} -> {}", key, value);
+            });
+            return chain.filter(exchange);
+        };
+    }
+
+    public static class Config {
+        private boolean enabled;
+
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
+        }
+    }
+}
---
+++ src/main/java/com/hsy/spring/cloud/gateway/filter/SecondGlobalFilter.java
@@ -0,0 +1,27 @@
+package com.hsy.spring.cloud.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 全局过滤器
+ *
+ * @author HuoShengyu
+ * @version 1.0
+ * @date 2019-11-28 14:40:01
+ */
+@Slf4j
+@Order(4)
+@Component
+public class SecondGlobalFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("执行全局过滤器[{}]", this.getClass().getName());
+        return chain.filter(exchange);
+    }
+}
