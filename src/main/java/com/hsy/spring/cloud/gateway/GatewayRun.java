95f3884dc29b414362ce23ba5f823a7ce86b7e05
huoshengyu <915967363@qq.com>
2020-04-10 17:02:03 +0800

    HSY-顺序测试

--- src/main/java/com/hsy/spring/cloud/gateway/GatewayRun.java
+++ src/main/java/com/hsy/spring/cloud/gateway/GatewayRun.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
+/**
+ * 启动类
+ *
+ * @author HuoShengyu
+ * @version 1.0
+ * @date 2019-11-28 14:40:01
+ */
 @EnableWebFlux
 @SpringBootApplication
 public class GatewayRun {
